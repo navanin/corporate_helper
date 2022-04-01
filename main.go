8e2b9c65871b60c091d8de9c314028d47f917c38
avb <avb@ttit.local>
2022-04-01 15:35:05 +0700

    asd

--- main.go
+++ main.go
@@ -34,34 +34,36 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		tmpl, _ := template.ParseFiles("index.html")
 		tmpl.Execute(w, data)
+	} else {
+		file, handler, err := r.FormFile("myFile")
+		if err != nil {
+			fmt.Println("Error Retrieving the File")
+			fmt.Println(err)
+			return
+		}
+
+		defer file.Close()
+		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+		fmt.Printf("File Size: %+v\n", handler.Size)
+		fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+		// Create file
+		dst, err := os.Create("files/" + handler.Filename)
+		defer dst.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Copy the uploaded file to the created file on the filesystem
+		if _, err := io.Copy(dst, file); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tmpl, _ := template.ParseFiles("index.html")
+		tmpl.Execute(w, data)
 	}
 	// Get handler for filename, size and headers
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
-	}
 
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	// Create file
-	dst, err := os.Create("files/" + handler.Filename)
-	defer dst.Close()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Copy the uploaded file to the created file on the filesystem
-	if _, err := io.Copy(dst, file); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	tmpl, _ := template.ParseFiles("index.html")
-	tmpl.Execute(w, data)
 }
